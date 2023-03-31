8eccf598d9634faa9e930ae70ed91b5bd8b5c035
DavidEsdrs <carpina1983@gmail.com>
2023-03-31 07:53:22 -0300

    Bubble sort

---
+++ README.md
@@ -0,0 +1,23 @@
+# Python studies
+This project was made in order to understand the main python concepts using selection and searching algorithms.
+
+## Concepts
+- List comprehension:
+    * Extract a list from another;
+    * Elegant syntax;
+    * Example:
+    ```py
+    ordered_list = [1, 2, 3, 4, 5]
+    square = [x ** 2 for x in ordered_list] # [1, 4, 9, 16, 25]
+    ```
+- Lambda:
+    * Allow write one liners functions;
+    * Compact functions;
+    * Example:
+    ```py
+    ordered_list = [1, 2, 3, 4, 5]
+    reversed_list = reversed(ordered_list)
+    square = lambda n: [x ** 2 for x in n]
+    squared_orderd_list = square(ordered_list) # [1, 4, 9, 16, 25]
+    squared_reversed_list = square(reversed_list) # [25, 16, 9, 4, 1]
+    ```
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import random
+
 # List comprehension
+any_numbers = random.sample(range(1, 1000), 50)
 lista_ordenada_5 = [1, 2, 3, 4, 5]
 lista_ordenada_50 = list(range(1, 51))
 lista_inversa_5 = [5, 4, 3, 2, 1]
@@ -13,21 +16,30 @@ odd_numbers = lambda n: [x for x in n if x % 2 != 0]
 square = lambda n: [x ** 2 for x in n]
 half = lambda n: [x // 2 for x in n]
 
-# List sort
+# Selection sort
+# O(n^2)
 def selection_sort(list_to_sort):
-    copy_list = list_to_sort[:]
-    for i in range(len(copy_list)):
+    for i in range(len(list_to_sort)):
         min_value_idx = i
-        for j in range(i, len(copy_list)):
-            if copy_list[j] < copy_list[min_value_idx]:
+        for j in range(i + 1, len(list_to_sort)):
+            if(list_to_sort[j] < list_to_sort[min_value_idx]):
                 min_value_idx = j
         if i != min_value_idx:
-            aux = copy_list[i]
-            copy_list[i] = copy_list[min_value_idx]
-            copy_list[min_value_idx] = aux
-    return copy_list
+            aux = list_to_sort[min_value_idx]
+            list_to_sort[min_value_idx] = list_to_sort[i]
+            list_to_sort[i] = aux
 
+# Bubble sort
+# O(n^2)
+def bubble_sort(list_to_sort):
+    n = len(list_to_sort)
+    for j in range(n - 1):
+        for i in range(n - 1):
+            if list_to_sort[i] > list_to_sort[i + 1]:
+                list_to_sort[i], list_to_sort[i + 1] = list_to_sort[i + 1], list_to_sort[i]
 
 if __name__ == '__main__':
-    list = selection_sort(lista_repetida_50)
-    print(half(lista_ordenada_50))
+    list = any_numbers
+    print(f"random: {list}")
+    bubble_sort(list)
+    print(F"sorted: {list}")
